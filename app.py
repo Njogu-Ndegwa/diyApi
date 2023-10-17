4389e7b5c46ed3f358d30145e37daab8f3968ba1
Dennis <dennisndegwa001@gmail.com>
2023-10-17 10:34:14 +0300

    Make minor changes

--- app.py
+++ app.py
@@ -10,8 +10,6 @@ from api.route.test import test_api
 from api.route.authController.delete import delete_api
 from api.route.diyController.saveSso import save_sso_api
 from api.route.paymentController.payments import payment_api
-from api.route.paymentController.sendMpesa import send_mpesa_api
-from api.route.paymentController.payWithCard import card_payment_api
 from api.route.paymentController.verifyPayment import verify_payment_api
 from api.route.diyController.generate_sso import generate_sso_api
 from api.route.diyController.getSite import get_sites_api
@@ -51,8 +49,6 @@ def create_app():
     app.register_blueprint(delete_api, url_prefix='/api')
     app.register_blueprint(save_sso_api, url_prefix='/api')
     app.register_blueprint(payment_api, url_prefix='/api')
-    app.register_blueprint(send_mpesa_api, url_prefix='/api')
-    app.register_blueprint(card_payment_api, url_prefix='/api')
     app.register_blueprint(verify_payment_api, url_prefix='/api')
     app.register_blueprint(generate_sso_api, url_prefix='/api')
     app.register_blueprint(get_sites_api, url_prefix='/api')
