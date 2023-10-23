612a3240a43f296da7941ea992115ec6776d814b
Dennis <dennisndegwa001@gmail.com>
2023-10-23 09:37:37 +0300

    Update Domain Problem

--- api/route/diyController/publishApi.py
+++ api/route/diyController/publishApi.py
@@ -22,23 +22,28 @@ publish_site_api = Blueprint('publish_site_api', __name__)
 
 def publish_site():
     site_name = request.json.get('site_name')
+    site_name = site_name.rstrip()
+    print(site_name)
     domain = request.json.get('domain')
     duda_endpoint = os.environ.get('DUDA_ENDPOINT')
     api_username = os.environ.get('DUDA_USERNAME')
     api_password = os.environ.get('DUDA_PASSWORD')
     url = f"{duda_endpoint}/sites/multiscreen/publish/{site_name}"
-    print('--------------------------------')
-    print(site_name, flush=True)
-    print('--------------------------------')
+    print(url, 'Ur---30')
+    print(domain, 'The Domain')
+    # url = "https://api.eu.duda.co/api/sites/multiscreen/publish/b22a80065f39432aa9d9c1f60cfa23ca"
+
     headers = {
         "accept": "application/json",
         "User-Agent": "Africa 118"
         }
 
     response = requests.post(url, headers=headers, auth=(api_username, api_password))
+    print(response, 'The Response')
     payload = { "site_domain": domain }
     if response.status_code == 204:
         update_url = f"{duda_endpoint}/sites/multiscreen/update/{site_name}"
+        print(update_url, '----44---')
         response = requests.post(update_url, json=payload, headers=headers, auth=(api_username, api_password))
         print('---------------------')
         print(response, flush=True)
